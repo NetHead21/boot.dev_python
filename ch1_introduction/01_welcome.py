93e842609cf71f47c07bf34ae4401037f4c43b1b
nethead21 <junivensaavedra@gmail.com>
2025-05-11 04:56:42 +0800

    Added Chapter 1

---
+++ ch1_introduction/01_welcome.py
@@ -0,0 +1,11 @@
+# Welcome to Learn Python
+# Python programming language
+
+# Let's Build a Game
+# During this course you'll build features for a text-based RPG called "Fantasy Quest".
+
+# Assignment
+# Press the gray Run ▶ button to run the code I wrote for you
+# Press the gold Submit ▶ button to run and submit the code
+# Press the --> right arrow at the top-right of the screen to go to the next lesson.
+print("Welcome to Fantasy Quest!")
---
+++ ch1_introduction/12_game_statistics.py
@@ -0,0 +1,7 @@
+# Game Statistics
+# The marketing team is asking for some stats about our Fantasy Quest game servers. They'd like to know the average number of players across our 4 servers.
+
+# Assignment
+# Debug the code so that it correctly prints the solution to the math problem.
+# print("(250 + 241 + 244 + 255) / 4")
+print((250 + 241 + 244 + 255) / 4)
