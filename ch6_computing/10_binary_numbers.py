099178147a9f5ea3543d331e1503ccb09c3ea040
nethead21 <junivensaavedra@gmail.com>
2025-05-13 22:56:17 +0800

    Added other lessons for chapter 6

---
+++ ch6_computing/10_binary_numbers.py
@@ -0,0 +1,63 @@
+# Binary Numbers
+# https://en.wikipedia.org/wiki/Binary_number
+# Binary numbers are just "base 2" numbers. They work the same way as "normal" base 10 numbers, but with two symbols instead of ten.
+
+# Base-2 (binary) symbols: 0 and 1
+# Base-10 (decimal) symbols: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+# binary
+
+# Each 1 in a binary number represents an ever-greater multiple of 2. In a 4-digit number, that means you have the eights place, the fours place, the twos place, and the ones place. Similar to how in decimal you would have the thousands place, the hundreds place, the tens place, and the ones place.
+
+
+# Binary in Python
+# You can write an integer in Python using binary syntax using the 0b prefix:
+
+# print(0b0001)
+# # Prints 1
+
+# print(0b0101)
+# # Prints 5
+
+# What does the binary number 1100 represent in decimal?
+
+
+# 8
+
+
+# 15
+
+
+# 13
+
+
+#     12
+
+
+# Binary 1101 is equivalent to which number in decimal?
+
+
+# 8
+
+
+# 16
+
+
+#     13
+
+
+# 12
+
+
+# What's the largest decimal number you can store in 5 digits of binary?
+
+
+# 64
+
+
+# 63
+
+
+# 32
+
+
+#     31
