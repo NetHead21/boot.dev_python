44ee2940227e5ecc7294d620c05c8748120556bf
nethead21 <junivensaavedra@gmail.com>
2025-05-14 22:52:57 +0800

    Added Chapter 7 and added some modules

---
+++ ch7_comparisons/03_comparison_practice.py
@@ -0,0 +1,13 @@
+# Comparison Practice
+# car_size = 4
+# truck_size = 5
+# is_smaller = car_size < truck_size
+# # is_smaller is True
+
+# Assignment
+# Complete the can_withstand_blow function. It should return True if the hero's armor is greater than or equal
+# to the damage dealt by the enemy, and False otherwise.
+
+
+def can_withstand_blow(hero_armor, enemy_damage):
+    return hero_armor >= enemy_damage
