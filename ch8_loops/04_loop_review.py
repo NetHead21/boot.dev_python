ddedf335fd6a137185efd46a891a3f79601edec8
nethead21 <junivensaavedra@gmail.com>
2025-05-15 00:57:34 +0800

    Added Chapter 8 and created some module

---
+++ ch8_loops/04_loop_review.py
@@ -0,0 +1,24 @@
+# Loop Review
+# for i in range(0, 10):
+#     print(i)
+
+# Prints the numbers 0-9 to the console.
+
+# Why would you want to use a loop?
+#     To do the same thing over and over without writing the same code
+# Because the computer understands loops better than repetitive code
+# Because loops speed up your app
+
+
+# Loop Review
+# for i in range(0, 1000):
+#     print(i)
+
+# Prints the numbers 0-999 to the console.
+
+# Why does the code on the left print 0-999 and not 0-1000?
+
+# Because print(i) only happens while i is less than or equal to 1000
+#     Because print(i) only happens while i is less than (not equal to) 1000
+# Because print(i) only happens while i is greater than or equal to 1000
+# Because print(i) only happens while i is greater than (not equal to) 1000
---
+++ ch8_loops/06_whitespace_in_python.py
@@ -0,0 +1,11 @@
+# Whitespace in Python
+# The body of a for-loop must be indented, otherwise you'll get a syntax error. Not only that, but every line in the body of the loop must be indented in the same way - we use the "4 spaces" convention. Pressing the <tab> key should automatically insert 4 spaces.
+
+# Whitespace matters in Python.
+
+
+# It is ____ to indent the body of a traditional for-loop in Python
+
+
+#     Required
+# Optional
